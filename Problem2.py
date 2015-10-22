e9a9dd4716ba3e1f4275429075be72390ea39961
tpe0002 <tpe0002@auburn.edu>
2015-10-22 00:12:08 -0500

    Done

--- Problem2.py
+++ Problem2.py
@@ -1,11 +1,10 @@
 import sys
 import lap
-import Problem1
 from Problem1 import swap
+import lap_h2
 
 def main():
     # initialize parameters then check for command line changes
-    show_intermediate = 1
     fname = 'r.csv'
     if len(sys.argv) > 1:
         fname = sys.argv[1]
@@ -13,27 +12,19 @@ def main():
             show_intermediate = int(sys.argv[2])
     # initialize the data structures using the cost matrix file
     costs, people, tasks = lap.initialize(fname, 1)
-    for k in range(len(people)):
-        # find the low cost task for this person
-        task, min_cost = lap.low_cost_task(costs, k, tasks)
-        # assign task to person and person to task
-        people[k] = task
-        tasks[task] = k
-    while True:
+    # initial solution with the "natural" sequence
+    cost = lap_h2.solve(costs, people, tasks, range(len(people)))
+    solution = lap.store_solution(people, tasks, cost, range(len(people)))
+    people = solution['people']
+    for i in range(len(people)-1):
         minval = 0
-        swapi = -1
-        swapj = -1
-        for i in range(len(people)-1):
-            for j in range(i+1,len(people)):
-                k = people[i]
-                l = people[j]
-                change = -costs[i][k] - costs[j][l] + costs[i][l] + costs[j][k]
-                if change < minval:
-                    minval = change
-                    swapi = i
-                    swapj = j
-                if minval<0:
-                    swap(i,j, people)
+        for j in range(i+1,len(people)):
+            k = people[i]
+            l = people[j]
+            change = -costs[i][k] - costs[j][l] + costs[i][l] + costs[j][k]
+            if change < minval:
+                minval = change
+                swap(i,j, people)
     print '\nFinal solution:'
     lap.show_solution(costs, people, tasks)
 
